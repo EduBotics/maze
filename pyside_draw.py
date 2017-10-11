a13e981b282ffbf62246b9cd222066519f6985ac
Michael Noel Jones <mnjones+git@gmail.com>
2017-10-11 17:38:09 +0100

    Converted memory to linked list and corrected rerun code

--- pyside_draw.py
+++ pyside_draw.py
@@ -17,6 +17,121 @@ import random
 
 from PySide import QtGui, QtCore
 from PySide.QtGui import QPixmap, QImage, QColor, QPalette
+from collections import namedtuple
+
+
+class Location(object):
+    LOCATION = namedtuple("Location", ["x", "y"])
+    CACHE = {}
+
+    def __new__(cls, *args):
+        x, y = args
+        location = cls.LOCATION(x, y)
+        if location in cls.CACHE:
+            return cls.CACHE[location]
+        else:
+            return super(Location, cls).__new__(cls, *args)
+
+    def __init__(self, x, y):
+        location = self.LOCATION(x, y)
+        self._location = location
+        self.CACHE[location] = self
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
+    @property
+    def x(self):
+        return self._location.x
+
+    @property
+    def y(self):
+        return self._location.y
+
+    def __call__(self):
+        return self._location
+
+    def __add__(self, adjustment):
+        try:
+            return Location(
+                self.x + adjustment.x,
+                self.y + adjustment.y
+            )
+        except AttributeError:
+            try:
+                return Location(
+                    self.x + adjustment[0],
+                    self.y + adjustment[1]
+                )
+            except IndexError:
+                raise
+
+
+class LinkedList(object):
+    class LinkedListItem(object):
+        def __init__(self, value=None, next=None, prev=None):
+            self._next = next
+            self._prev = prev
+            self._value = value
+
+        @property
+        def next(self):
+            return self._next
+
+        @next.setter
+        def next(self, item):
+            self._next = item
+
+        @property
+        def prev(self):
+            return self._prev
+
+        @prev.setter
+        def prev(self, item):
+            self._prev = item
+
+        @property
+        def value(self):
+            return self._value
+
+        @value.setter
+        def value(self, val):
+            self._value = val
+
+    def __init__(self, root=None):
+        self._root = self.LinkedListItem(value=root)
+        self._head = None
+
+    def __str__(self):
+        out = ""
+        item = self.root
+        out += item.value
+        while item.next:
+            item = item.next
+            out += ";{}".format(item.value)
+        return out
+
+    @property
+    def root(self):
+        return self._root
+
+    @property
+    def head(self):
+        if self._head is None:
+            item = self.root
+            while item.next:
+                item = item.next
+            self._head = item
+        return self._head
+
+    @head.setter
+    def head(self, item):
+        self._head = item
+
+    def append(self, value):
+        item = self.head
+        item.next = self.LinkedListItem(value=value, prev=item)
+        self._head = item.next
 
 
 class Robot(object):
@@ -25,30 +140,35 @@ class Robot(object):
 
     def __init__(self, maze=None):
         self.maze = maze
-        self.location = (1, 0)
-        self.memory = []
+        self.location = Location(1, 0)
+        self.memory = LinkedList(root=self.location)
         self.corridors = {}
         self.visits = {}
 
     def move(self):
+        self.memory.append(self.location)
         if self.location not in self.corridors:
             corridors = self.sense_corridors()
             self.corridors[self.location] = corridors
             self.visits[self.location] = 1
         else:
             if self.visits[self.location] > 1:
-                while len(self.memory) and self.memory[-1] != self.location:
-                    print "{}: {}".format(self.location, self.memory)
-                    del(self.memory[-1])
+                print "Backtracking..."
+                item = self.memory.head.prev
+                print "Checking {}".format(item.value)
+                while item.value != self.location:
+                    item = item.prev
+                    print "Checking {}".format(item.value)
+                self.memory.head = item.prev
+                print "Set head to {}".format(self.memory.head.value)
+
             corridors = self.corridors[self.location]
             self.visits[self.location] += 1
-        print self.location, corridors
-        self.memory.append(self.location)
 
         if len(corridors):
             direction, adjustment = corridors[0]
             self.corridors[self.location] = corridors[1:]
-            self.location = (self.location[0] + adjustment[0], self.location[1] + adjustment[1])
+            self.location += adjustment
         else:
             print "Blocked!"
             exit()
@@ -56,8 +176,8 @@ class Robot(object):
     def sense_corridors(self):
         corridors = []
         for direction, adjustment in zip(self.DIRECTIONS, self.ADJUSTMENTS):
-            test_loc = (self.location[0] + adjustment[0], self.location[1] + adjustment[1])
-            dest_pix = self.maze.getpixel(*test_loc)
+            test_loc = self.location + adjustment
+            dest_pix = self.maze.getpixel(test_loc.x, test_loc.y)
             if dest_pix == self.maze.white:
                 corridors.append((direction, adjustment))
         return corridors
@@ -69,13 +189,18 @@ class Maze(QtGui.QWidget):
         super(Maze, self).__init__()
         self.speed = 500
         self.boxsize = boxsize
-        self.screenheight = int(math.floor(self.contentsRect().height() / self.boxsize))
-        self.screenwidth = int(math.floor(self.contentsRect().width() / self.boxsize))
+        self.screenheight = int(
+            math.floor(self.contentsRect().height() / self.boxsize)
+        )
+        self.screenwidth = int(
+            math.floor(self.contentsRect().width() / self.boxsize)
+        )
         self.white = self.palette().color(QPalette.Active, QPalette.Window)
         self.search = True
 
         self.maze = []
         self.robot = Robot(self)
+        self.end_location = Location(self.screenwidth - 1, self.screenheight)
 
         # Initialise
         self.timer = QtCore.QBasicTimer()
@@ -83,7 +208,11 @@ class Maze(QtGui.QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, self.contentsRect().width() + self.boxsize, self.contentsRect().height() + self.boxsize)
+        self.setGeometry(
+            300, 300,
+            self.contentsRect().width() + self.boxsize,
+            self.contentsRect().height() + self.boxsize
+        )
         self.setWindowTitle('Points')
         self.show()
         self.timer.start(self.speed, self)
@@ -115,7 +244,7 @@ class Maze(QtGui.QWidget):
             self.drawSquare(qp, i, j, 2)
 
     def draw_robot(self, qp):
-        self.drawSquare(qp, self.robot.location[0], self.robot.location[1], 3)
+        self.drawSquare(qp, self.robot.location.x, self.robot.location.y, 3)
 
     def build_maze(self):
         random.seed()
@@ -124,7 +253,7 @@ class Maze(QtGui.QWidget):
                 if random.random() < 0.25:
                     if (i < 4 and j < 4):
                         continue
-                    if (i > self.screenwidth - 4 and j > self.screenheight - 4):
+                    if (i > self.screenwidth - 4 and j > self.screenheight - 4):  # noqa
                         continue
                     self.maze.append((i, j))
 
@@ -163,22 +292,22 @@ class Maze(QtGui.QWidget):
 
     def timerEvent(self, event):
         if self.search:
-            if self.robot.location != (self.screenwidth - 1, self.screenheight):
+            if (self.robot.location != self.end_location):
                 self.robot.move()
             else:
                 # Exit found
                 print "Complete"
                 print "Rerun..."
                 self.search = False
-                self.memory_index = 0
+                self.memory_index = self.robot.memory.root
         else:
-            if self.memory_index == len(self.robot.memory):
+            self.robot.location = self.memory_index.value
+            print "MEM: {}".format(self.memory_index.value)
+            if self.memory_index.next:
+                self.memory_index = self.memory_index.next
+            else:
                 print "Completed"
                 exit()
-            else:
-                self.robot.location = self.robot.memory[self.memory_index]
-                print "MEM: {}: LOC: {}".format(self.memory_index, self.robot.location)
-                self.memory_index += 1
         self.update()
 
     def getpixel(self, x, y):
